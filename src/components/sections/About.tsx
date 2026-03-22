95883ce3ad2816d012b33e4b86d2f4dbf0bb4a9a
PrevinBogopa <previnbogopa@icloud.com>
2026-03-22 03:38:31 +0200

    Create About.tsx

---
+++ src/components/sections/About.tsx
@@ -0,0 +1,7 @@
+const ROLES = ['Cloud Engineer', 'DevOps Engineer', 'AI Developer', 'Solutions Architect', 'Cloud Consultant']
+export default function About() {
+  return (
+    <section id="about" className="relative bg-white overflow-hidden">
+    </section>
+  )
+}
